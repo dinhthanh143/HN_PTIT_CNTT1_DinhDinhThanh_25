882039987b130593d39ed60de824a291bceff488
dinhthanh143 <neonlimea@gmail.com>
2025-09-09 23:30:01 +0700

    Update CustomLink.tsx

--- src/pages2/CustomLink.tsx
+++ src/pages2/CustomLink.tsx
@@ -3,14 +3,21 @@ import { Link, useNavigate } from "react-router-dom";
 
 export const CustomLink = () => {
   const navigate = useNavigate();
- 
+   const handleRedirect = (): void => {
+    if (window.location.href === "http://localhost:5173/home-page") {
+      navigate("/home-page"); 
+    } else {
+      navigate("/not-found"); 
+    }
+  };
   return (
     <div>
       <h3>CustomLink</h3>
-      {window.location.href === "http://localhost:5173/home-page"?       <Link to={"/home-page"} >To homepage</Link>
- :        <Link to={"/not-found"} >To notfound</Link>
+      {window.location.href === "http://localhost:5173/home-page"?       <Link to={"/home-page"} onClick={handleRedirect}>To homepage</Link>
+ :        <Link to={"/not-found"} onClick={handleRedirect}>To notfound</Link>
 }
     </div>
   );
 };
+
 
